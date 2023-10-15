f56d463e0f35738043ca77abe60714ab7a7437f8
PrishitaRayapura <138791236+PrishitaRayapura@users.noreply.github.com>
2023-10-15 21:55:01 +0530

    Add files via upload

---
+++ CTOF.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Function to convert Celsius to Fahrenheit
+double celsiusToFahrenheit(double celsius) {
+    return (celsius * 9.0 / 5.0) + 32.0;
+}
+
+int main() {
+    double celsius;
+
+    cout << "Enter temperature in degrees Celsius: ";
+    cin >> celsius;
+
+    double fahrenheit = celsiusToFahrenheit(celsius);
+
+    cout << "Temperature in Fahrenheit: " << fahrenheit << "°F" << endl;
+
+    return 0;
+}
+
